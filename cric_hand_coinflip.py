6d3b8f099461e7cf982222f4467ad698f9594770
Amay Krishna <blahbam247@gmail.com>
2023-02-22 17:02:43 +0530

    perfected the game, working on the score display

--- cric_hand_coinflip.py
+++ cric_hand_coinflip.py
@@ -2,6 +2,9 @@
 import random
 from time import sleep
 
+score_bat = 0
+score_comp = 0
+
 user_batting_done = False
 user_bowling_done = False
 
@@ -107,9 +110,11 @@ sleep(2)
 
 
 if coinflip_win_status_bat == True: 
+    user_batting_done = True
     def coinflip_win_status_bat_act():
         
-        score = 0
+        global user_score
+        user_score = 0
 
 
         '1' == 1
@@ -140,11 +145,11 @@ if coinflip_win_status_bat == True:
                     if user_roll in keywords and user_roll != "score":
                         for char in user_roll:
                             if char.isdigit():
-                                score += int(char)
+                                user_score += int(char)
                                 print("You typed",user_roll)
 
                     if user_roll == "score":
-                        print("Your score is:",score)
+                        print("Your score is:",user_score)
 
                     if user_roll == comp_choice:
                         sleep(2)
@@ -156,7 +161,8 @@ if coinflip_win_status_bat == True:
                         sleep(1)
                         print("OUT!!")
                         sleep(1)
-                        print("Your total score was:",score)
+                        print("Your total score was:",user_score)
+                    
 
                         break
 
@@ -166,15 +172,19 @@ if coinflip_win_status_bat == True:
             else:
                 print("invalid. numbers are only alllowed.")
     coinflip_win_status_bat_act()
+    user_score = score_bat
 
 if coinflip_win_status_bowl == True:
+    user_bowling_done = True
     def coinflip_win_status_bowl_act():
 
         keywords = ['1','2','3','4','5','6','score']
 
         comp_roll = ['1','2','3','4','5','6']
 
-        score = 0
+        global comp_score
+        comp_score = 0
+        
 
 
         print("The computer will be batting. Your objective is to match the computer's roll to knock it out.")
@@ -189,11 +199,11 @@ if coinflip_win_status_bowl == True:
                     if comp_choice_bat != user_roll_bowl and user_roll_bowl != "score":
                         for char in comp_choice_bat:
                                 if char.isdigit():
-                                    score += int(char)
+                                    comp_score += int(char)
                         print("You typed",user_roll_bowl,"and the computer rolled",comp_choice_bat)
 
                     elif user_roll_bowl == "score":
-                        print("The score is",score)
+                        print("The score is",comp_score)
 
                     elif comp_choice_bat == user_roll_bowl:
                         sleep(2)
@@ -207,8 +217,65 @@ if coinflip_win_status_bowl == True:
                         sleep(1)
                         print("OUT!!")
                         sleep(1)
-                        print("The total score was:",score)
-                        user_bowling_done = True
+                        print("The total score was:",comp_score)
+                        break
+
+                        
+
+
+
+                else:
+                    print("Invalid. Try again.")
+                
+
+            else:
+                print("invalid. Try again.")
+    coinflip_win_status_bowl_act()
+    comp_score = score_comp
+
+if user_batting_done == True:
+    def coinflip_win_status_bowl_act():
+
+        keywords = ['1','2','3','4','5','6','score']
+
+        comp_roll = ['1','2','3','4','5','6']
+
+        global comp_score
+        comp_score = 0
+        
+
+
+        print("The computer will be batting. Your objective is to match the computer's roll to knock it out.")
+
+        while True:
+            user_roll_bowl = input("Type a number from 1 to 6 ")
+
+            if  type(user_roll_bowl) == str:
+                if user_roll_bowl in keywords:
+                    comp_choice_bat = random.choice(comp_roll)
+
+                    if comp_choice_bat != user_roll_bowl and user_roll_bowl != "score":
+                        for char in comp_choice_bat:
+                                if char.isdigit():
+                                    comp_score += int(char)
+                        print("You typed",user_roll_bowl,"and the computer rolled",comp_choice_bat)
+
+                    elif user_roll_bowl == "score":
+                        print("The score is",comp_score)
+
+                    elif comp_choice_bat == user_roll_bowl:
+                        sleep(2)
+                        print("Yay! You and the computer rolled a", user_roll_bowl)
+                        sleep(2)
+                        print("YOU")
+                        sleep(1)
+                        print("KNOCKED")
+                        sleep(1)
+                        print("THE COMPUTER")
+                        sleep(1)
+                        print("OUT!!")
+                        sleep(1)
+                        print("The total score was:",comp_score)
                         break
 
                         
@@ -223,6 +290,82 @@ if coinflip_win_status_bowl == True:
                 print("invalid. Try again.")
     coinflip_win_status_bowl_act()
 
+if user_bowling_done == True:
+    def coinflip_win_status_bat_act():
+        
+        global user_score
+        user_score = 0
 
 
+        '1' == 1
+        '2' == 2
+        '3' == 3
+        '4' == 4
+        '5' == 5
+        '6' == 6
+
+
+
+        keywords = ['1','2','3','4','5','6','score']
+
+        comp_roll = ['1','2','3','4','5','6']
+
+        print("The computer will be bowling. Your objective is to keep scoring without typing a number that would match the computer's roll.")
+
+
+        while True:
+            user_roll = (input("Its your turn! Type a number from 1 to 6!"))
+
+            if type(user_roll) == str:
+
+                comp_choice = random.choice(comp_roll)
+
+                if user_roll in keywords:
+
+                    if user_roll in keywords and user_roll != "score":
+                        for char in user_roll:
+                            if char.isdigit():
+                                user_score += int(char)
+                                print("You typed",user_roll)
+
+                    if user_roll == "score":
+                        print("Your score is:",user_score)
+
+                    if user_roll == comp_choice:
+                        sleep(2)
+                        print("Uh Oh! You and the computer rolled a", user_roll)
+                        sleep(2)
+                        print("YOU")
+                        sleep(1)
+                        print("ARE")
+                        sleep(1)
+                        print("OUT!!")
+                        sleep(1)
+                        print("Your total score was:",user_score)
+                    
+
+                        break
+
+                else:
+                    print("Invalid. Try again.")
+                    
+            else:
+                print("invalid. numbers are only alllowed.")
+    coinflip_win_status_bat_act()
+
+print("computing the final results...")
+sleep(2)
+
+print("The score of the computer:",score_comp)
+print("And your score:",user_score)
+
+print("Therefore...")
+
+if score_comp > user_score:
+
+    print("You lost!")
+
+elif score_comp < user_score:
+
+    print("Congratulations!! You Won!")
 
